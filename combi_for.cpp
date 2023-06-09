66daf830540171a7c8e9b1f30641c2f18009a599
kingof7 <vlfl0@naver.com>
2023-06-10 02:22:18 +0900

    조합(combination)

---
+++ combi_for.cpp
@@ -0,0 +1,24 @@
+// ÁßÃ¸for·Î Çª´Â Á¶ÇÕ
+#include<bits/stdc++.h>
+using namespace std;
+
+int n = 5, k = 3, a[5] = {1, 2, 3, 4, 5};
+int main() {
+	vector<int> b;
+//	for(int i = 0; i < n; i++) {
+//		for(int j = 0; j < i; j++) {
+//			for(int k = 0; k < j; k++) {
+//				cout << i << " : " << j << " : " << k << "\n";
+//			}
+//		}
+//	}
+	for(int i = 0; i < n; i++) {
+		for(int j = i + 1; j < n; j++) {
+			for(int k = j + 1; k < n; k++) {
+				cout << "index : " << i << " : " << j << " : " << k << "\n";
+				cout << a[i] << ", " << a[j] << ", " << a[k] << "\n";
+			}
+		}
+	}
+	return 0;
+}
