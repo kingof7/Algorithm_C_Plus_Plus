c78572245775adb4e336013fc099b8049275e3bd
kingof7 <vlfl0@naver.com>
2023-06-10 00:15:49 +0900

    순열 : 재귀함수로 만드는 순열

---
+++ recurse_permutation.cpp
@@ -0,0 +1,35 @@
+// 순열 재귀적으로 풀이
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> v; 
+void printV(vector<int> &v) {
+	for(int i = 0; i < v.size(); i++) {
+		cout << v[i] < " ";
+	}
+	cout << "\n";
+} 
+void makePermutation(int n, int r, int depth) {
+	cout << n << " : " << r << " : " << depth << '\n'; 
+	if(r == depth) {
+		//logic
+		printV(v);
+		return;
+	}
+	for(int i = depth; i < n; i++) {
+		swap(v[i], v[depth]);
+		makePermutation(n, r, depth + 1); // depth는 재귀깊이 
+		swap(v[i], v[depth]);
+	}
+	return;
+}
+int main() {
+	/* 재귀적으로 풀이 */
+	for(int i = 1; i <= 3; i++) v.push_back(i);
+	makePermutation(3, 3, 0);
+	
+	/* C++ 내장함수 (next_permutation 활용 풀이) 
+	do {
+	}while(next_permutation(v.begin(), v.end()))
+	*/
+	return 0;
+}
